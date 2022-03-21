275ad32453930e1c4ae5df3c6847c6f89ff1a1b6
Boonyarit Wiriyagulpat <boonyarit@ekoapp.com>
2022-03-21 23:43:54 +0700

    chore(test): try to make a mistake

--- libs/services/messages/src/lib/services-messages.spec.ts
+++ libs/services/messages/src/lib/services-messages.spec.ts
@@ -2,6 +2,6 @@ import { getMessage } from './services-messages';
 
 describe('getMessage', () => {
   it('should work', () => {
-    expect(getMessage()).toEqual('Hello from message service');
+    expect(getMessage()).toEqual('Hello X message service');
   });
 });
